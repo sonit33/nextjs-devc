83b8b3073b1b4df4b21559fad9a492ce337ecbec
Sonit <sonitbuilds+git@gmail.com>
2023-03-22 21:45:55 +0000

    tailwind

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,18 @@
 import * as React from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
+import Banner from "../components/Banner";
+import FeatureSection from "../components/FeatureSection";
+import NavigationBar from "../components/NavigationBar";
+import StackedFlyoutMenu from "../components/StackedFlyoutMenu";
 
 const Index = () => {
-	return <div>Welcome to Next.js</div>;
+  return (
+    <>
+      <Banner />
+      <NavigationBar />
+      <StackedFlyoutMenu />
+      <FeatureSection />
+    </>
+  );
 };
 
 export default Index;
